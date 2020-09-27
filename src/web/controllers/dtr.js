bdba5425dd18c92963a370780b906ed61c92a26e
Gregory Daynes <greg.daynes@pixelunion.net>
2020-09-27 00:44:57 -0700

    Add time-record view

--- src/web/controllers/dtr.js
+++ src/web/controllers/dtr.js
@@ -10,8 +10,26 @@ module.exports = {
 async function index (req, res) {
   debug('index', req.params)
 
+  const project = await new Project()
+    .get('d74d6590-923c-48e3-89b5-952a24ea9fd2')
+
+  const departments = await new Departments()
+    .all({ projectId: project.id })
+    .then(res => res.map(department => ({ key: department.id, value: department.name })))
+
+  const agents = await new Agents()
+    .all({ projectId: project.id })
+    .then(res => res.map(agent => ({
+      name: agent.name,
+      position: agent.position,
+      department: agent.department
+    })))
+
   res.view = 'time-record.pug'
   res.locals = {
+    agents,
+    departments,
+    project,
     title: 'new'
   }
 }
