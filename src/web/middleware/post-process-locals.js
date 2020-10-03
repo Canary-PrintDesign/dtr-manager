3b7922d2024d49f4e8e3ee1cfeadd5996d947c0e
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-02 19:48:30 -0700

    Feature post process locals middleware
    Add middleware layer capable of doing operations on the locals of a
    response before rendering

---
+++ src/web/middleware/post-process-locals.js
@@ -0,0 +1,25 @@
+const debug = require('lib/debug')('http:api:middleware:postProcessLocals')
+
+module.exports = postProcessLocalsInit
+
+function postProcessLocalsInit () {
+  return postProcessLocals
+}
+
+async function postProcessLocals (req, res, next) {
+  debug(res.locals)
+
+  try {
+    const locals = res.locals
+    const { project } = req.context
+
+    res.locals = {
+      ...locals,
+      title: `${locals.title} - ${project.name}`
+    }
+
+    next()
+  } catch (err) {
+    next(err)
+  }
+}
