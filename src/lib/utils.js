de34d2cbfa11d2278820a7103b2111e7f94931ee
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-06 16:28:46 -0700

    Chore document util functions

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,13 +1,21 @@
-exports.compose = (...fns) => (input) => fns.reduceRight((v, fn) => v.then(fn), Promise.resolve(input))
+// Function Composition
 
-exports.toString = (data) => Promise.resolve(data).then(res => res.toString())
+// compose
+// create and return a function built up of functions (right to left)
+// first function can receive any number of arguments, all other
+// functions require a single argument format
+exports.compose = (...fns) =>
+  (input) =>
+    fns.reduceRight((v, fn) => v.then(fn), Promise.resolve(input))
 
-exports.pipe = function pipe (...fns) {
-  return function pipe (input) {
-    return fns.reduce((v, fn) => v.then(fn), Promise.resolve(input))
-  }
-}
+// pipe
+// like compose but left to right composition of functions
+exports.pipe = (...fns) =>
+  (input) =>
+    fns.reduce((v, fn) => v.then(fn), Promise.resolve(input))
 
-exports.pipeWith = function pipe (x, ...fns) {
-  return exports.pipe(...fns)(x)
-}
+// pipeWith
+// used as a shorthand for pipe, that takes the first argument and
+// passes it down, this is equivalent to pipe(...fns)(data)
+exports.pipeWith = (x, ...fns) =>
+  exports.pipe(...fns)(x)
