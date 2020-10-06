30438b046f4cba3387ef63d8cf56294828bbc9d1
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-06 16:12:49 -0700

    Chore remove :debug from debugging statements

--- src/lib/debug.js
+++ src/lib/debug.js
@@ -3,5 +3,5 @@ const Debug = require('debug')
 module.exports = function setup (namespace, options) {
   if (options && options.enable) Debug.enable(options.enable)
 
-  return Debug(`dtr:${namespace}:debug`)
+  return Debug(`dtr:${namespace}`)
 }
