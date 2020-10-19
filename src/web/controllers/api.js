82badcb914abbc59e2460f1ad8268703292ab64d
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-18 18:24:05 -0700

    Chore refactor controllers - new component design

--- src/web/controllers/api.js
+++ src/web/controllers/api.js
@@ -1,18 +1,54 @@
 const debug = require('lib/debug')('http:web:controller:api')
-const Department = require('components/department')
+const Agent = require('components/agent')
 const AuthToken = require('components/auth-token')
+const { pipeWith } = require('lib/utils')
 
 module.exports = {
-  requestAuthToken
+  requestAuthToken,
+  requestAgents
 }
 
 async function requestAuthToken (req, res) {
   debug('requestAuthToken', req.query)
 
-  const department = await new Department().get(req.query.department)
-  const authToken = await new AuthToken({ department: department.id }).save()
+  const { department } = req.query
+
+  const authToken = await pipeWith(
+    { department },
+    AuthToken.create(),
+    AuthToken.generateToken(),
+    AuthToken.setExpiryDate(),
+    AuthToken.save()
+  )
+
+  debug('requestAuthToken', authToken)
 
   res.data = {
     token: await authToken.token
   }
+}
+
+async function requestAgents (req, res) {
+  debug('requestAgents', req.params, req.body, req.query)
+
+  const { project } = req.context
+  const agents = await getAgents({
+    projectId: project.id,
+    departmentId: req.query.department
+  })
+
+  res.data = { agents }
+}
+
+// Private
+
+async function getAgents ({ projectId, departmentId }) {
+  const agents = await Agent.findAll(projectId, departmentId)
+
+  return Promise.all(agents)
+    .then(res => res.map(agent => ({
+      name: agent.name,
+      position: agent.position,
+      department: agent.department
+    })))
 }
