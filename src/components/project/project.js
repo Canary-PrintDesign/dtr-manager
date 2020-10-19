7ed21f113dbde7b84ed802cd759d05e71fb2cb46
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-18 18:18:27 -0700

    Chore refactor project component

---
+++ src/components/project/project.js
@@ -0,0 +1,33 @@
+const Repo = require('./project-repo')
+const Component = require('lib/component')
+
+exports.create = create()
+function create () {
+  return async (props = {}) => ({
+    id: props.id,
+    logo: props.logo,
+    name: props.name,
+    hostname: props.hostname,
+    startDate: props.startDate || props.start_date,
+    status: props.status || 'unpublished'
+  })
+}
+
+exports.findWith = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.findWith(repo, create(), props)
+
+exports.save = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.save(repo, create(), await serialize(props))
+
+function serialize (props = {}) {
+  const data = {
+    ...props,
+    start_date: props.startDate
+  }
+
+  delete data.startDate
+
+  return data
+}
