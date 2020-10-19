cd01a55319606f40b498fcc042524eff61632c17
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-18 18:19:54 -0700

    Chore refactor time-record component

---
+++ src/components/time-record/index.js
@@ -0,0 +1,7 @@
+const TimeRecord = require('./time-record')
+
+module.exports = {
+  create: TimeRecord.create(),
+  findWith: TimeRecord.findWith(),
+  save: TimeRecord.save()
+}
---
+++ src/components/time-record/time-record.js
@@ -0,0 +1,42 @@
+const Repo = require('./time-record-repo')
+const Component = require('lib/component')
+
+exports.create = create
+function create () {
+  return async (props = {}) => ({
+    id: props.id,
+    project: props.project,
+    department: props.department,
+    agent: props.agent,
+    date: props.date || Date.now(),
+    workStart: props.workStart || props.work_start,
+    workStop: props.workStop || props.work_stop,
+    lunchStart: props.lunchStart || props.lunch_start,
+    lunchStop: props.lunchStop || props.lunch_stop
+  })
+}
+
+exports.findWith = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.findWith(repo, create(), props)
+
+exports.save = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.save(repo, create(), await serialize(props))
+
+function serialize (props) {
+  const data = {
+    ...props,
+    work_start: props.workStart,
+    work_stop: props.workStop,
+    lunch_start: props.lunchStart,
+    lunch_stop: props.lunchStop
+  }
+
+  delete data.workStart
+  delete data.workStop
+  delete data.lunchStart
+  delete data.lunchStop
+
+  return data
+}
