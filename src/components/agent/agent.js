75187e365f2b2a6b31f4ffe0e09b874469debf42
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-18 18:19:07 -0700

    Chore refactor agent(s) component

---
+++ src/components/agent/agent.js
@@ -0,0 +1,25 @@
+const Component = require('lib/component')
+const Repo = require('./agent-repo')
+
+exports.create = create()
+function create () {
+  return async (props = {}) => ({
+    id: props.id,
+    project: props.project,
+    department: props.department,
+    name: props.name || '',
+    position: props.position || ''
+  })
+}
+
+exports.findAll = (repo = Repo) =>
+  async (project, department) =>
+    await Component.findAll(repo, create(), { project, department })
+
+exports.findWith = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.findWith(repo, create(), props)
+
+exports.save = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.save(repo, create(), await Component.serialize(props))
