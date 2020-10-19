9d144c35cb08787fd60554f97fb1df89090e1b6c
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-18 18:19:29 -0700

    Chore refactor record-note component

---
+++ src/components/record-note/record-note.js
@@ -0,0 +1,25 @@
+const Repo = require('./record-note-repo')
+const Component = require('lib/component')
+
+exports.create = create
+function create () {
+  return async (props = {}) => ({
+    id: props.id,
+    project: props.project,
+    department: props.department,
+    note: props.note || '',
+    date: props.date
+  })
+}
+
+exports.findAll = (repo = Repo) =>
+  async (project) =>
+    await Component.findAll(repo, create(), { project })
+
+exports.findWith = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.findWith(repo, create(), props)
+
+exports.save = (repo = Repo) =>
+  async (props = {}) =>
+    await Component.save(repo, create(), await Component.serialize(props))
