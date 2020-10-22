72e0ff746e96bc974a8c9ef7a68e1ebc85eaeba6
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-21 21:57:24 -0700

    Feature component utils add create, schema, types

--- src/lib/component.js
+++ src/lib/component.js
@@ -1,18 +1,31 @@
-exports.findAll = async (repo, create, props) => {
+const Joi = require('joi')
+
+exports.findAll = async (repo, fn, props) => {
   const data = await repo.findAll({ ...props })
-  return data.map(async item => await create(item))
+  return data.map(async item => await fn(item))
 }
 
-exports.findWith = async (repo, create, props) => {
+exports.findWith = async (repo, fn, props) => {
   const data = await repo.findByProp({ ...props })
     .catch(() => {})
 
-  return await create({ ...data })
+  return await fn({ ...data })
 }
 
-exports.save = async (repo, create, props) => {
+exports.save = async (repo, fn, props) => {
   const storedData = await repo.store(props)
-  return create({ ...storedData })
+  return fn({ ...storedData })
 }
 
 exports.serialize = (props = {}) => ({ ...props })
+
+exports.create = async (schema, props = {}) =>
+  await schema.validateAsync(props, { stripUnknown: true })
+
+exports.schema = (schema) =>
+  Joi.object(schema)
+
+exports.types = {
+  guid: Joi.string().guid(),
+  string: Joi.string()
+}
