09a74124fc51d356fd6135c4bf2d11ad7fb510dd
Gregory Daynes <greg.daynes@pixelunion.net>
2020-10-21 21:56:37 -0700

    Fix incorrect param in envars for testing

--- src/lib/env.js
+++ src/lib/env.js
@@ -3,7 +3,7 @@ const env = process.env
 module.exports = {
   dbName: isTest() ? env.PSQL_TEST_DB : env.PSQL_DB,
   dbHost: isTest() ? env.PSQL_TEST_HOST : env.PSQL_HOST,
-  dbUsername: isTest() ? env.PSQL_TEST_HOST : env.PSQL_USER,
+  dbUsername: isTest() ? env.PSQL_TEST_USER : env.PSQL_USER,
   dbPassword: isTest() ? env.PSQL_TEST_PASSWORD : env.PSQL_PASSWORD,
   dbPort: isTest() ? env.PSQL_TEST_PORT : env.PSQL_PORT,
   dbSSL: (env.PSQL_SSL === 'true'),
