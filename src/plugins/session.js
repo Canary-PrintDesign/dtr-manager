71d091ef03eed1b4ac7ee7e6f7783ba21ea3a5bd
Gregory Daynes <gregdaynes@gmail.com>
2021-09-05 13:31:11 -0700

    feature: implement stateless cookie session

---
+++ src/plugins/session.js
@@ -0,0 +1,14 @@
+const fp = require('fastify-plugin')
+const fs = require('fs')
+const path = require('path')
+
+module.exports = fp(async (fastify) => {
+  fastify.register(require('fastify-secure-session'), {
+    key: fs.readFileSync(path.join(__dirname, '..', '..', 'secret-key')),
+    cookie: {
+      path: '/',
+      httpOnly: true,
+      signed: true,
+    },
+  })
+})
